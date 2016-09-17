9ce83d3ef3f066f4dd75be993defb67ffb31641a
dantegg <luroufan327@hotmail.com>
2016-09-17 20:39:38 +0800

    update

--- controllers/static.js
+++ controllers/static.js
@@ -1,3 +1,12 @@
+const fs = require('fs')
+const mime = require('mime');
+const sendFile = require('../utils/send')
+const joinPath = require('path').join
+const publicPath = joinPath(__dirname,'../public')
+
 module.exports = function(req,res){
-  res.end('static')
+  //console.log('params',req.params)
+  var path = req.params[1]
+  path = joinPath(publicPath,path)
+  sendFile(path,res)
 }
