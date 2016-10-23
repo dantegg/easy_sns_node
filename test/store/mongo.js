bfef927174ba7600e3fcbad56f2e5cdf463c2f40
dantegg <luroufan327@hotmail.com>
2016-10-23 17:08:53 +0800

    update with add mongodb

---
+++ test/store/mongo.js
@@ -0,0 +1,22 @@
+/**
+ * Created by dantegg on 2016/10/23.
+ */
+
+
+const { MongoClient } = require('mongodb')
+const { expect } = require('chai')
+
+describe('Mongo',()=>{
+    let db
+    before(async ()=>{
+        db = await MongoClient.connect('mongodb://localhost/testdb')
+        await db.dropDatabase()
+    })
+
+    it('should insert and find document', async ()=>{
+        const fooColl = db.collection('foo')
+        const insertResult = await fooColl.insertOne({foo:"bar"})
+        const result =  await fooColl.findOne({_id:insertResult.insertedId})
+        expect(result.foo).to.equal('bar')
+    })
+})
